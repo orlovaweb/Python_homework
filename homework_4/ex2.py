027c56b32202af01bc751670b67979b31fc63e90
l.orlova <igimon@mail.ru>
2023-03-04 12:22:30 +0300

    Fix ex2

--- homework_4/ex2.py
+++ homework_4/ex2.py
@@ -8,8 +8,8 @@ lst = [random.randrange(10) for _ in range(n)]
 print(lst)
 
 max_harvest = 0
-for i in range(len(lst)):
-    if (i == (len(lst)-1)):
+for i in range(n):
+    if (i == (n-1)):
         harvest = lst[i-1]+lst[i]+lst[0]
     else:
         harvest = lst[i-1]+lst[i]+lst[i+1]
