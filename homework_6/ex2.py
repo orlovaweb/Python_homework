e6f8c0ae2913ce0f0cbe130a3465b5392a1aa23a
l.orlova <igimon@mail.ru>
2023-03-10 11:45:36 +0300

    homework_6 is done

---
+++ homework_6/ex2.py
@@ -0,0 +1,10 @@
+import random
+n = int(input("Введите количество элементов массива: "))
+min = int(input("Введите минимум отрезка: "))
+max = int(input("Введите максимум отрезка: "))
+
+lst = [random.randrange(30) for _ in range(n)]
+print(lst)
+for i in range(n):
+    if min < lst[i] < max:
+        print(i, end=" ")
