77cd3be4b306360f5f76f8abf8df2194a2cd138d
l.orlova <igimon@mail.ru>
2023-03-13 09:24:17 +0300

    Fix homework_7

--- homework_7/ex2.py
+++ homework_7/ex2.py
@@ -2,10 +2,7 @@
 
 def print_operation_table(operation, num_rows=6, num_columns=6):
     for i in range(1, num_rows+1):
-        listOfRow = []
-        for j in range(1, num_columns+1):
-            listOfRow.append(operation(i, j))
-        print(*listOfRow, sep="\t")
+        print(*list(map(lambda j: operation(i, j), range(1, num_columns+1))), sep="\t")
 
 
 print_operation_table(lambda x, y: x * y)
